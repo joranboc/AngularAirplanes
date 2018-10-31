832f2a30d4518cb0087fbeed3a090bdac09d0923
Kuruk Studios <kurukstudios@iMac-de-Kuruk.local>
2018-10-30 21:26:37 -0500

    RouterOulet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { NgModule, Component } from '@angular/core';
+import { RouterModule,Routes } from '@angular/router';
+import { PlanesComponent} from './planes/planes.component'
+const routes: Routes = [
+  {path: 'planes', component: PlanesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
